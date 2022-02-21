b03e8c58465ed5a7eb8dd6f7c5206a3bcf7d0f36
Pavel <pkrylov@gmail.com>
2022-02-21 17:24:17 +0300

    New commit

---
+++ src/main/java/com/rpa/rpaplatform/RpaplatformApplication.java
@@ -0,0 +1,15 @@
+package com.rpa.rpaplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpaplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpaplatformApplication.class, args);
+
+		System.out.println("hello! 1");
+	}
+
+}
